183b0850eab655e108b59220b91931ac749bb716
1YusifBedel1 <tu7hqak2t@code.edu.az>
2024-04-04 19:25:56 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+// let array=[66,3,2,34,1,65,40,21,30]
+// let cutededler=1
+// let tekedler=1
+
+// for (let i = 0; i < array.length; i++) {
+//     if(array[i]%2==0){
+//         cutededler*=array[i]
+//     }
+//     else{
+//         tekedler*=array[i]
+//     }
+    
+// }
+// if(cutededler > tekedler){
+//     console.log("cutler boyukdur")
+// }
+// else if(cutededler =tekedler){
+//     console.log("beraberdiler")
+// }
+// else{
+//     console.log("tekler boyukdur")
+// }
+
+// let array=[66,3,2,34,1,65,40,21,30]
+// let minEded=array[0]
+// let maxEded=array[0]
+// for (let i = 0; i < array.length; i++) {
+//     if(array[i]>maxEded){
+//         maxEded=array[i]
+//     }
+//     else if(array[i]<minEded){
+//         minEded=array[i]
+//     }
+// }
+// console.log(maxEded)
+// console.log(minEded)
+
+// let array=[66,3,2,34,1,65,40,21,30]
+// tekeded=0
+
+// for (let i = 0; i < array.length; i++) {
+//     if(array[i]%2==1){
+//         tekeded+=array[i]
+//     }
+// }
+// console.log(tekeded);
+
+// let array=[66,3,2,34,1,65,40,21,30]
+// let eded=0
+// counter=0
+// for (let i = 0; i < array.length; i++) {
+//     if(array[i]%3==0){
+//         eded+=array[i]
+//         counter++
+//     }
+    
+// }
+// console.log(eded/counter)
+
+// let array=[66,3,2,34,1,65,40,21,30]
+// count=0
+// for (let index = 0; index < array.length; index++) {
+//     if(array[index]>99){
+//         count++
+//     }
+    
+// }
+// console.log(count)
+
+// let array=[66,3,2,34,1,65,40,21,30]
+// eded=0
+// for (let i = 0; i < array.length; i++) {
+//     if(array[i]==34){
+//         console.log(i)
+//     }
+// }
+
+// let array=[66,3,2,34,1,-65,40,-21,30]
+// let menfi=0
+// let musbet=0
+// for (let i = 0; i < array.length; i++) {
+//     if(array[i]>0){
+//         musbet++
+//     }
+//     else if(array[i]<0){
+//         menfi++
+//     }
+// }
+// if(menfi > musbet){
+//     console.log("menfiler coxdur")
+// }
+// else if(menfi<musbet){
+//     console.log("musbetler coxdur")
+// }
+// else{
+//     console.log("beraberdiler")
+// }
